8df83312f8be2783248163436aa21ae8318b9b2b
ranjit-das <Ranjit.Das@ons.gov.uk>
2019-07-26 13:30:21 +0100

    LU-2998: Types and their processing added for VetQandResponse, validationRequest

--- classes.go
+++ classes.go
@@ -1,34 +1,54 @@
 package main
 
-type ContributorResponses struct{
+type ContributorResponses struct {
 	Reference string `json:"reference"`
-	Period string `json:"period"`
-	Survey string `json:"survey"`
-	BpmID string `json:"instance"`
+	Period    string `json:"period"`
+	Survey    string `json:"survey"`
+	BpmID     string `json:"instance"`
 
 	Responses []responseData `json:"responses"`
 }
 
-type responseData struct{
+type responseData struct {
 	QuestionCode string `json:"questionCode"`
-	Response string `json:"response"`
+	Response     string `json:"response"`
 }
 
-type validationConfig struct{
-	questionCode string
+type validationConfig struct {
+	questionCode        string
 	derivedQuestionCode string
 }
 
-type isFound struct{
-	qCodeIsFound bool
+type isFound struct {
+	qCodeIsFound  bool
 	dqCodeIsFound bool
 }
 
-type sendStruct struct{
+type sendStruct struct {
 	Reference string `json:"reference"`
-	Period string `json:"period"`
-	Survey string `json:"survey"`
-	BpmID string `json:"instance"`
+	Period    string `json:"period"`
+	Survey    string `json:"survey"`
+	BpmID     string `json:"instance"`
+}
 
+type validationRequest struct {
+	PrimaryValue    string `json:"primaryValue"`
+	ComparisonValue string `json:"comparisonValue"`
+	MetaData        string `json:"metaData"`
+}
 
+type validationResult struct {
+	QuestionCode string
+	ValueFormula string
+	Triggered    string
+}
+
+type VetQandResponse struct {
+	FinalQuestCode         string
+	FinalDerivedQuestCode  string
+	FinalQuestCodeValue    string
+	FinalDerivedQuestValue string
+	IsQuestionCodeFound    bool
+	IsDerivedQuestFound    bool
+	ValidationResult       validationResult
 }
--- main.go
+++ main.go
@@ -1,61 +1,74 @@
 package main
 
-import(
+import (
 	"encoding/json"
-	"os"
-	"github.com/aws/aws-lambda-go/lambda"
-	"sync"
 	"fmt"
+	"os"
+	"sync"
+
+	"github.com/aws/aws-lambda-go/lambda"
 )
 
-func handle(inputData ContributorResponses){
+func handle(inputData ContributorResponses) {
 	var config []validationConfig
 	config = append(config, validationConfig{questionCode: "601", derivedQuestionCode: "700"})
 	config = append(config, validationConfig{questionCode: "602", derivedQuestionCode: "701"})
 	loopOverConfig(config, inputData)
 }
 
-func main(){
+func main() {
 	lambda.Start(handle)
 }
 
-func loopOverConfig(configData []validationConfig, responses ContributorResponses){
+func loopOverConfig(configData []validationConfig, responses ContributorResponses) {
 	var wg sync.WaitGroup
 	fmt.Printf("There are %d elements in config\n", len(configData))
 	wg.Add(len(configData))
-	for i:=0; i< len(configData); i++ {
-		var found isFound
-		found.dqCodeIsFound = false
-		found.qCodeIsFound = false
-		go loopOverResponse(responses.Responses, configData[i], found, &wg)
+	for i := 0; i < len(configData); i++ {
+		// var found isFound
+		// found.dqCodeIsFound = false
+		// found.qCodeIsFound = false
+		var vetQandResponse VetQandResponse
+		go loopOverResponse(responses.Responses, configData[i], vetQandResponse, &wg)
 	}
 	wg.Wait()
 }
 
-func loopOverResponse(responses []responseData, configData validationConfig, found isFound, waitGroup *sync.WaitGroup){
-	for i:=0; i< len(responses); i++{
-		if processQuestionCode(responses[i], configData.questionCode){
-			found.qCodeIsFound = true
+func loopOverResponse(responses []responseData, configData validationConfig, vetQandResponse VetQandResponse, waitGroup *sync.WaitGroup) {
+	for i := 0; i < len(responses); i++ {
+		if processQuestionCode(responses[i], configData.questionCode) {
+			vetQandResponse.IsQuestionCodeFound = true
+			vetQandResponse.FinalQuestCode = responses[i].QuestionCode
+			vetQandResponse.FinalQuestCodeValue = responses[i].Response
 		}
-		
-		if processQuestionCode(responses[i], configData.derivedQuestionCode){
-			found.dqCodeIsFound = true
+
+		if processQuestionCode(responses[i], configData.derivedQuestionCode) {
+			vetQandResponse.IsDerivedQuestFound = true
+			vetQandResponse.FinalDerivedQuestCode = responses[i].QuestionCode
+			vetQandResponse.FinalDerivedQuestValue = responses[i].Response
 		}
 		fmt.Printf("responses: %s\n", responses[i])
-		fmt.Printf("found struct: %s\n", found)
-		if bothQAndDqFound(found){
+
+		// fmt.Printf("found struct: %s\n", found)
+		if vetQandResponse.IsQuestionCodeFound && vetQandResponse.IsDerivedQuestFound {
 			var data PostData
-			fmt.Printf("sending: %+v\n", responses[i])
-			output, err := json.Marshal(responses[i])
-			if err != nil{
+			var valReq validationRequest
+			valReq.PrimaryValue = vetQandResponse.FinalQuestCodeValue
+			valReq.ComparisonValue = vetQandResponse.FinalDerivedQuestValue
+			fmt.Printf("sending: %+v\n", valReq)
+			output, err := json.Marshal(valReq)
+			if err != nil {
 				println("An error occured while marshaling: %s", err)
 			}
-			found.dqCodeIsFound = false
-			found.qCodeIsFound = false
+			// found.dqCodeIsFound = false
+			// found.qCodeIsFound = false
 			data.Data = output
 			data.LambdaLocation = os.Getenv("lambda")
 			makePost(data)
+			break
+
 		}
 	}
+	fmt.Println("A statement just after for loop.")
 	waitGroup.Done()
 }
